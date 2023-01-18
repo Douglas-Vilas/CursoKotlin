bc81a80fcfe744a57f54793d496881c9acb40289
Douglas Vilas <douglas_vilass@hotmail.com>
2023-01-18 18:35:22 -0300

    Update Main.kt
    Usando a estrutura "when" ou invés da estrutura "if", para simplificar o nosso código e deixá-lo com um visual melhor.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,9 +8,15 @@ fun main() {
     var balance = 0.0 // Variable that can change its initial value.
     balance = 100 + 2.0 // Or 100.0
     balance += 200
+    balance -= 1000
 
     println("Titular: $titular") // In Kotlin, we can use the "$" to use the variable inside the string field.
     println("Account Number: $accountNumber")
     println("Account Balance: $balance")
 
+    when {
+        balance > 0.0 -> println("Account is positive.")
+        balance == 0.0 -> println("Account is neutral.")
+        else -> println("Account is negative")
+    }
 }
