7c8b15e0294a2789696506a4151f96d432dd12ca
Douglas Vilas <douglas_vilass@hotmail.com>
2023-03-20 12:10:56 -0300

    ContinuaçãoTecnicasDeImportsEApelidos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,11 @@
+import model.Client
+import newmodel.Client as NewClient
 import test.testsEmployees
 
 fun main() {
     // Design of a digital Bank
-    testsEmployees()
-}
+    val client = Client(name = "Douglas", cpf = "", password = 1)
+    val newClient = NewClient()
 
 
 
